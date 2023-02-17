a9d8453d89515d630acdeb3c5aa12dd6ac7f140a
Viper292 <crazymelon@hotmail.es>
2023-02-17 03:16:36 -0300

    Mezcla cartas y agrega copias
    Se agregan copias de las caras para el juego y un mezclador para garantizar que sean distintas las posiciones cada vez que se juega.

--- juego.js
+++ juego.js
@@ -20,6 +20,48 @@ function comenzarJuego(){
     mostrarEstadisticas();
     reiniciarTiempo();
     interval = setInterval(aumentarSegundos, 1000);
+
+    ponerImagenEnGrilla(cuadrosAleatorios());
+}
+
+const caras = [
+    {id: "card-1", name: 'card1', img: 'img/cara-1.jpg'},
+    {id: "card-2", name: 'card2', img: 'img/cara-2.jpg'},
+    {id: "card-3", name: 'card3', img: 'img/cara-3.jpg'},
+    {id: "card-4", name: 'card4', img: 'img/cara-4.jpg'},
+    {id: "card-5", name: 'card5', img: 'img/cara-5.jpg'},
+    {id: "card-6", name: 'card6', img: 'img/cara-6.jpg'},
+    {id: "card-7", name: 'card7', img: 'img/cara-7.jpg'},
+    {id: "card-8", name: 'card8', img: 'img/cara-8.jpg'},
+    {id: "card-9", name: 'card9', img: 'img/cara-a.jpg'},
+    {id: "card-10", name: 'card10', img: 'img/cara-b.jpg'},
+    {id: "card-11", name: 'card11', img: 'img/cara-c.jpg'},
+    {id: "card-12", name: 'card12', img: 'img/cara-d.jpg'},
+    {id: "card-13", name: 'card13', img: 'img/cara-e.jpg'},
+    {id: "card-14", name: 'card14', img: 'img/cara-f.jpg'},
+    {id: "card-15", name: 'card15', img: 'img/cara-g.jpg'},
+    {id: "card-16", name: 'card16', img: 'img/cara-h.jpg'},
+];
+
+function cuadrosAleatorios(){
+    const randomArray = caras.slice();
+    for (let i = randomArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [randomArray[i], randomArray[j]] = [randomArray[j], randomArray[i]];
+    }
+    return randomArray;
+}
+
+function ponerImagenEnGrilla(cuadrosAleatorios){
+    const grilla = document.querySelectorAll('.card')
+    
+    grilla.forEach(function(cuadrado, index){
+        cuadrado.addEventListener('click', function(){
+            const cara = cuadrosAleatorios[index];
+
+            cuadrado.innerHTML = `<img src="${cara.img}">`
+        })
+    })
 }
 
 function aumentarSegundos(){
