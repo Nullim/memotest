3f77b6b8a75de51be03432930fdc8b66604e87c8
Viper292 <crazymelon@hotmail.es>
2023-04-21 07:10:58 -0300

    Cambia la funcionalidad de los tests

--- cypress/e2e/memotest.cy.js
+++ cypress/e2e/memotest.cy.js
@@ -1,9 +1,27 @@
 const URL = '127.0.0.1:8080'
 
-context('Memotest', () => {
+function cardClicker(cards, currentIndex = 0){
+  if (currentIndex === 16){
+    return;
+  }
+  let currentCard = cards[currentIndex];
+  let secondCounter = 1;
+  while(!currentCard.classList.contains('correcto')){
+    cy.get(currentCard).click();
+    cy.get(cards[secondCounter]).click();
+    currentCard = cards[currentIndex];
+    secondCounter++;
+    if (secondCounter === 16){
+      break;
+    }
+  }
+  cardClicker(cards, currentIndex + 1);
+}
 
+context('Memotest', () => {
     before(() => {
         cy.visit(URL);
+        cy.clearCookies();
     });
 
     describe('Juega al memotest', () =>{
@@ -13,25 +31,22 @@ context('Memotest', () => {
             cy.get('#tablero').find('.card').should('have.length', NUMERO_CUADROS);
         });
 
-
-        // Esta prueba será incorrecta en un promedio de 1 en cada 16 ejecuciones.
         it('Se asegura que los cuadros sean aleatorios', () => {
           cy.visit(URL);
-        
           cy.get('#nuevo-juego').click();
-        
-          cy.get('#card-1').click().then(($card) => {
-            cy.wrap($card).find('img').invoke('attr', 'src').then((seleccion1) => {
-              cy.visit(URL);
-              cy.get('#nuevo-juego').click();
-        
-              cy.get('#card-1').click().then(($card) => {
-                cy.wrap($card).find('img').invoke('attr', 'src').then((seleccion2) => {
-                  expect(seleccion1).not.to.equal(seleccion2);
-                });
-              });
-            });
-          });
+          const firstCard = cy.get('#card-1').click().find('img').invoke('attr', 'src');
+          const secondCard = cy.get('#card-2').click().find('img').invoke('attr', 'src');
+
+          cy.reload()
+          cy.get('#nuevo-juego').click();
+          const newFirstCard = cy.get('#card-1').click().find('img').invoke('attr', 'src');
+          const newSecondCard = cy.get('#card-2').click().find('img').invoke('attr', 'src').then(() => {
+            if (firstCard != newFirstCard){
+              expect(firstCard).not.to.equal(newFirstCard)
+            } else{
+              expect(secondCard).not.to.equal(newSecondCard);
+            }
+          })
         });
 
         it('Termina el juego y muestra felicitaciones', () => {
@@ -41,26 +56,7 @@ context('Memotest', () => {
           cy.get('.card').as('cards');
         
           cy.get('@cards').then((cards) => {
-            for (let i = 0; i < cards.length; i++) {
-              if (!Cypress.$(`#card-${i+1}`).hasClass('correcto')) {
-                cy.get(`#card-${i+1}`).click();
-                
-                if (!Cypress.$(`#card-${i+1}`).hasClass('correcto')) {
-                  cy.get('@cards').each((card, j) => {
-                    if (i != j && !Cypress.$(card).hasClass('correcto')) {
-                      cy.get(card).click();
-                      cy.get(card).find('img').invoke('attr', 'src').then((src2) => {
-                        cy.get(`#card-${i+1}`).click().find('img').invoke('attr', 'src').then((src1) => {
-                          if (src1 === src2) {
-                            cy.get(`#card-${j+1}`).should('have.class', 'correcto');
-                          }
-                        });
-                      });
-                    }
-                  });
-                }
-              }
-            }
+            cardClicker(cards);
           });
           cy.get('#felicitaciones').should('be.visible');
         });
@@ -76,39 +72,3 @@ context('Memotest', () => {
         })
     })
 })
-
-/*
-it('Termina el juego', () => {
-          cy.visit(URL);
-          cy.get('#nuevo-juego').click();
-
-          const cartas = Array.from(document.querySelectorAll('.card'))
-
-          for(let i = 0; i < NUMERO_CUADROS; i++){
-            let cartaActual = cy.get(`#card-${i+1}`)
-            cartas.forEach(function (cartaActual, i){
-              cartaActual.click().then((cartaActual) => {
-                cy.wrap(cartaActual).find('img').invoke('attr, src').then((cartaActual) =>{
-                  cy.get(cartas[i]).click().then(($segundaCarta) =>{
-                    cy.wrap($segundaCarta).find('img').invoke('attr, src')
-
-                    if (i === 0){
-                      return;
-                    }
-
-                    if (cartaActual != $segundaCarta){
-                      cy.wait(1000)
-                      return;
-                    }
-
-                    if (cartaActual === $segundaCarta){
-                      return;
-                    }
-                  })
-                })
-              });
-            })
-          }
-          cy.get('#felicitaciones').should('be.visible');
-        })
-*/
